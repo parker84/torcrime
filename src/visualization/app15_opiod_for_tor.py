353517b06d258c0bc680214e0663cb6cf168b496
bryparker <brydon.parker@mail.utoronto.ca>
2020-04-25 16:58:04 -0400

    toronto map showing in opiodd style app - still no interactivity though

--- src/visualization/app15_opiod_for_tor.py
+++ src/visualization/app15_opiod_for_tor.py
@@ -1,7 +1,12 @@
 import os
 import pathlib
 import re
-
+import plotly.express as px
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import json
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -23,67 +28,13 @@ server = app.server
 
 APP_PATH = str(pathlib.Path(__file__).parent.resolve())
 
-df_lat_lon = pd.read_csv(
-    os.path.join(APP_PATH, os.path.join("data", "lat_lon_counties.csv"))
-)
-df_lat_lon["FIPS "] = df_lat_lon["FIPS "].apply(lambda x: str(x).zfill(5))
-
-df_full_data = pd.read_csv(
-    os.path.join(
-        APP_PATH, os.path.join("data", "age_adjusted_death_rate_no_quotes.csv")
-    )
-)
-df_full_data["County Code"] = df_full_data["County Code"].apply(
-    lambda x: str(x).zfill(5)
-)
-df_full_data["County"] = (
-    df_full_data["Unnamed: 0"] + ", " + df_full_data.County.map(str)
-)
+with open("./data/raw/rates/Neighbourhood_Crime_Rates_Boundary_File_.json", "r") as f:
+    counties = json.load(f)
+df = pd.read_csv("./data/raw/rates/Neighbourhood_Crime_Rates_Boundary_File_.csv",
+                dtype={"Hood_ID": str})
 
 YEARS = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
 
-BINS = [
-    "0-2",
-    "2.1-4",
-    "4.1-6",
-    "6.1-8",
-    "8.1-10",
-    "10.1-12",
-    "12.1-14",
-    "14.1-16",
-    "16.1-18",
-    "18.1-20",
-    "20.1-22",
-    "22.1-24",
-    "24.1-26",
-    "26.1-28",
-    "28.1-30",
-    ">30",
-]
-
-DEFAULT_COLORSCALE = [
-    "#f2fffb",
-    "#bbffeb",
-    "#98ffe0",
-    "#79ffd6",
-    "#6df0c8",
-    "#69e7c0",
-    "#59dab2",
-    "#45d0a5",
-    "#31c194",
-    "#2bb489",
-    "#25a27b",
-    "#1e906d",
-    "#188463",
-    "#157658",
-    "#11684d",
-    "#10523e",
-]
-
-DEFAULT_OPACITY = 0.8
-
-mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNqdnBvNDMyaTAxYzkzeW5ubWdpZ2VjbmMifQ.TXcBE-xg9BFdV2ocecc_7g"
-mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
 
 # App layout
 
@@ -144,29 +95,19 @@ app.layout = html.Div(
                                 ),
                                 dcc.Graph(
                                     id="county-choropleth",
-                                    figure=dict(
-                                        data=[
-                                            dict(
-                                                lat=df_lat_lon["Latitude "],
-                                                lon=df_lat_lon["Longitude"],
-                                                text=df_lat_lon["Hover"],
-                                                type="scattermapbox",
-                                            )
-                                        ],
-                                        layout=dict(
-                                            mapbox=dict(
-                                                layers=[],
-                                                accesstoken=mapbox_access_token,
-                                                style=mapbox_style,
-                                                center=dict(
-                                                    lat=38.72490, lon=-95.61446
-                                                ),
-                                                pitch=0,
-                                                zoom=3.5,
-                                            ),
-                                            autosize=True,
-                                        ),
-                                    ),
+                                    figure=(
+                                        px.choropleth(df, 
+                                            geojson=counties, 
+                                            color="Assault_AVG",
+                                            locations="OBJECTID", 
+                                            featureidkey="properties.OBJECTID",
+                                            hover_data=["Neighbourhood"],
+                                            color_continuous_scale="Viridis",
+                                            scope="north america",
+                                        )
+                                        .update_geos(showcountries=False, showcoastlines=False, showland=False, fitbounds="locations")
+                                        .update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+                                    )
                                 ),
                             ],
                         ),
@@ -178,100 +119,100 @@ app.layout = html.Div(
 )
 
 
-@app.callback(
-    Output("county-choropleth", "figure"),
-    [Input("years-slider", "value")],
-    [State("county-choropleth", "figure")],
-)
-def display_map(year, figure):
-    cm = dict(zip(BINS, DEFAULT_COLORSCALE))
+# @app.callback(
+#     Output("county-choropleth", "figure"),
+#     [Input("years-slider", "value")],
+#     [State("county-choropleth", "figure")],
+# )
+# def display_map(year, figure):
+#     cm = dict(zip(BINS, DEFAULT_COLORSCALE))
 
-    data = [
-        dict(
-            lat=df_lat_lon["Latitude "],
-            lon=df_lat_lon["Longitude"],
-            text=df_lat_lon["Hover"],
-            type="scattermapbox",
-            hoverinfo="text",
-            marker=dict(size=5, color="white", opacity=0),
-        )
-    ]
+#     data = [
+#         dict(
+#             lat=df_lat_lon["Latitude "],
+#             lon=df_lat_lon["Longitude"],
+#             text=df_lat_lon["Hover"],
+#             type="scattermapbox",
+#             hoverinfo="text",
+#             marker=dict(size=5, color="white", opacity=0),
+#         )
+#     ]
 
-    annotations = [
-        dict(
-            showarrow=False,
-            align="right",
-            text="<b>Age-adjusted death rate<br>per county per year</b>",
-            font=dict(color="#2cfec1"),
-            bgcolor="#1f2630",
-            x=0.95,
-            y=0.95,
-        )
-    ]
+#     annotations = [
+#         dict(
+#             showarrow=False,
+#             align="right",
+#             text="<b>Age-adjusted death rate<br>per county per year</b>",
+#             font=dict(color="#2cfec1"),
+#             bgcolor="#1f2630",
+#             x=0.95,
+#             y=0.95,
+#         )
+#     ]
 
-    for i, bin in enumerate(reversed(BINS)):
-        color = cm[bin]
-        annotations.append(
-            dict(
-                arrowcolor=color,
-                text=bin,
-                x=0.95,
-                y=0.85 - (i / 20),
-                ax=-60,
-                ay=0,
-                arrowwidth=5,
-                arrowhead=0,
-                bgcolor="#1f2630",
-                font=dict(color="#2cfec1"),
-            )
-        )
+#     for i, bin in enumerate(reversed(BINS)):
+#         color = cm[bin]
+#         annotations.append(
+#             dict(
+#                 arrowcolor=color,
+#                 text=bin,
+#                 x=0.95,
+#                 y=0.85 - (i / 20),
+#                 ax=-60,
+#                 ay=0,
+#                 arrowwidth=5,
+#                 arrowhead=0,
+#                 bgcolor="#1f2630",
+#                 font=dict(color="#2cfec1"),
+#             )
+#         )
 
-    if "layout" in figure:
-        lat = figure["layout"]["mapbox"]["center"]["lat"]
-        lon = figure["layout"]["mapbox"]["center"]["lon"]
-        zoom = figure["layout"]["mapbox"]["zoom"]
-    else:
-        lat = (38.72490,)
-        lon = (-95.61446,)
-        zoom = 3.5
+#     if "layout" in figure:
+#         lat = figure["layout"]["mapbox"]["center"]["lat"]
+#         lon = figure["layout"]["mapbox"]["center"]["lon"]
+#         zoom = figure["layout"]["mapbox"]["zoom"]
+#     else:
+#         lat = (38.72490,)
+#         lon = (-95.61446,)
+#         zoom = 3.5
 
-    layout = dict(
-        mapbox=dict(
-            layers=[],
-            accesstoken=mapbox_access_token,
-            style=mapbox_style,
-            center=dict(lat=lat, lon=lon),
-            zoom=zoom,
-        ),
-        hovermode="closest",
-        margin=dict(r=0, l=0, t=0, b=0),
-        annotations=annotations,
-        dragmode="lasso",
-    )
+#     layout = dict(
+#         mapbox=dict(
+#             layers=[],
+#             accesstoken=mapbox_access_token,
+#             style=mapbox_style,
+#             center=dict(lat=lat, lon=lon),
+#             zoom=zoom,
+#         ),
+#         hovermode="closest",
+#         margin=dict(r=0, l=0, t=0, b=0),
+#         annotations=annotations,
+#         dragmode="lasso",
+#     )
 
-    base_url = "https://raw.githubusercontent.com/jackparmer/mapbox-counties/master/"
-    for bin in BINS:
-        geo_layer = dict(
-            sourcetype="geojson",
-            source=base_url + str(year) + "/" + bin + ".geojson",
-            type="fill",
-            color=cm[bin],
-            opacity=DEFAULT_OPACITY,
-            # CHANGE THIS
-            fill=dict(outlinecolor="#afafaf"),
-        )
-        layout["mapbox"]["layers"].append(geo_layer)
+#     base_url = "https://raw.githubusercontent.com/jackparmer/mapbox-counties/master/"
+#     for bin in BINS:
+#         geo_layer = dict(
+#             sourcetype="geojson",
+#             source=base_url + str(year) + "/" + bin + ".geojson",
+#             type="fill",
+#             color=cm[bin],
+#             opacity=DEFAULT_OPACITY,
+#             # CHANGE THIS
+#             fill=dict(outlinecolor="#afafaf"),
+#         )
+#         layout["mapbox"]["layers"].append(geo_layer)
 
-    fig = dict(data=data, layout=layout)
-    return fig
+#     fig = dict(data=data, layout=layout)
+#     return fig
 
 
-@app.callback(Output("heatmap-title", "children"), [Input("years-slider", "value")])
-def update_map_title(year):
-    return "Heatmap of age adjusted mortality rates \
-				from poisonings in year {0}".format(
-        year
-    )
+# @app.callback(Output("heatmap-title", "children"), [Input("years-slider", "value")])
+# def update_map_title(year):
+#     return "Heatmap of age adjusted mortality rates \
+# 				from poisonings in year {0}".format(
+#         year
+#     )
 
 
 if __name__ == "__main__":
