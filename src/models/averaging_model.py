72b3cb706382076116133ac56b269390cf80b335
bryparker <brydon.parker@mail.utoronto.ca>
2020-04-26 19:16:59 -0400

    predict model running and tested

--- src/models/averaging_model.py
+++ src/models/averaging_model.py
@@ -4,19 +4,21 @@ import pandas as pd
 
 class AvgModel(BaseHelpers):
 
-    def predict(self, df, days_of_potential_crime):
+    def predict(self, df, days_of_potential_crime, **kwargs):
         """[summary]
 
         Arguments:
             df {[type]} -- needs to have the follwing cols: 
                             ["neighbourhood"]
         """
+        super(AvgModel, self).__init__(**kwargs)
         self._check_cols(df)
         totals_per_nbhd = self._get_totals_per_nbhd(df)
         totals_per_nbhd["expected_crimes_per_day"] = (
             totals_per_nbhd["crimes_counts_per_nbhd"] / 
             days_of_potential_crime
         )
+        totals_per_nbhd["days_of_potential_crime"] = days_of_potential_crime
         return totals_per_nbhd
 
     def _check_cols(self, df):
