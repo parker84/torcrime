35881adbbd9afa5511bbe062b8f5a6fcaf6baea5
bryparker <brydon.parker@mail.utoronto.ca>
2020-04-26 18:51:39 -0400

    avg model running w tests

---
+++ src/models/averaging_model.py
@@ -0,0 +1,35 @@
+from dlearn.utils.base import BaseHelpers
+import pandas as pd
+
+
+class AvgModel(BaseHelpers):
+
+    def predict(self, df, days_of_potential_crime):
+        """[summary]
+
+        Arguments:
+            df {[type]} -- needs to have the follwing cols: 
+                            ["neighbourhood"]
+        """
+        self._check_cols(df)
+        totals_per_nbhd = self._get_totals_per_nbhd(df)
+        totals_per_nbhd["expected_crimes_per_day"] = (
+            totals_per_nbhd["crimes_counts_per_nbhd"] / 
+            days_of_potential_crime
+        )
+        return totals_per_nbhd
+
+    def _check_cols(self, df):
+        for col in ["neighbourhood"]:
+            assert col in df.columns, f"{col} missing"
+
+    def _get_totals_per_nbhd(self, df):
+        totals_per_nbhd = df.neighbourhood.value_counts()
+        return (
+            pd.DataFrame(totals_per_nbhd)
+            .reset_index()
+            .rename(columns={
+                "neighbourhood": "crimes_counts_per_nbhd",
+                "index": "neighbourhood"
+            })
+        )
