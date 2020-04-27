8798f543213bc87a3c343da4291c39d78163dff9
bryparker <brydon.parker@mail.utoronto.ca>
2020-04-27 10:35:56 -0400

    cleaning up geojsons

---
+++ src/data/clean_geojson.py
@@ -0,0 +1,8 @@
+import json
+
+with open('./data/raw/shapefile_toronto/Neighbourhood_Crime_Rates_Boundary_File_.json') as json_file:
+    data = json.load(json_file)
+    for feat in data["features"]:
+        feat["properties"]["clean_nbdh_id"] = int(feat["properties"]["Hood_ID"].lstrip("0"))
+with open('./data/raw/shapefile_toronto/Neighbourhood_Crime_Rates_Boundary_File_clean.json', "w") as out_file:
+    json.dump(data, out_file)
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,10 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 
+import pandas as pd
+from sqlalchemy import create_engine
+from decouple import config
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+engine = create_engine(config('ENGINE_PATH'))
+conn = engine.connect()
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+df = pd.read_sql("select * from stg.crime_data", conn)
+df.to_csv("./data/processed/crime_data.csv")
