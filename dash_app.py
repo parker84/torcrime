0546565c3faddbc4977cffc8d1b9f32663a9fc74
bryparker <brydon.parker@mail.utoronto.ca>
2021-03-09 16:48:23 -0500

    reformatitng the code and adding a title

--- dash_app.py
+++ dash_app.py
@@ -76,50 +76,50 @@ app.layout = html.Div(
     id="root",
     children=[
                 html.Div(
-            [
-                html.Div(
-                    [
-                        html.Img(
-                            src=app.get_asset_url("dash-logo.png"),
-                            id="plotly-image",
-                            style={
-                                "height": "60px",
-                                "width": "auto",
-                                "margin-bottom": "25px",
-                            },
-                        )
-                    ],
-                    className="one-third column",
-                ),
-                html.Div(
                     [
                         html.Div(
                             [
-                                html.H1(
-                                    "Crime Rates in Toronto",
-                                    style={"margin-bottom": "0px"},
-                                ),
-                                html.H3(
-                                    "Crime Counts and Probabilities by Neighbourhood", style={"margin-top": "0px"}
-                                ),
-                            ]
-                        )
-                    ],
-                    className="one-half column",
-                    id="title",
-                ),
-                html.Div(
-                    [
-                        html.A(
-                            html.Button("GitHub", id="github-button"),
-                            href="https://github.com/parker84/toronto-crime",
-                        )
-                    ],
-                    className="one-third column",
-                    id="button",
-                ),
-            ],
-            id="header",
+                                html.Img(
+                                    src=app.get_asset_url("dash-logo.png"),
+                                    id="plotly-image",
+                                    style={
+                                        "height": "60px",
+                                        "width": "auto",
+                                        "margin-bottom": "25px",
+                                    },
+                                )
+                            ],
+                            className="one-third column",
+                        ),
+                        html.Div(
+                            [
+                                html.Div(
+                                    [
+                                        html.H3(
+                                            "Crime Rates in Toronto",
+                                            style={"margin-bottom": "0px"},
+                                        ),
+                                        html.H5(
+                                            "Crime Counts and Probabilities by Neighbourhood", style={"margin-top": "0px"}
+                                        ),
+                                    ]
+                                )
+                            ],
+                            className="one-half column",
+                            id="title",
+                        ),
+                        html.Div(
+                            [
+                                html.A(
+                                    html.Button("GitHub", id="github-button"),
+                                    href="https://github.com/parker84/toronto-crime",
+                                )
+                            ],
+                            className="one-third column",
+                            id="button",
+                        ),
+                ],
+                id="header",
             className="row flex-display",
             style={"margin-bottom": "25px"},
         ),
@@ -228,6 +228,18 @@ app.layout = html.Div(
                                     value=['Outside']
                                 ), 
                             ],
+                        ),
+                        html.Div(
+                            [
+                                html.Div(
+                                    [
+                                        html.H5(
+                                            "Heatmaps"
+                                        ),
+                                    ]
+                                )
+                            ],
+                            id="heatmap-title-intro",
                         ),
                         html.Div(
                             id="heatmap-container",
