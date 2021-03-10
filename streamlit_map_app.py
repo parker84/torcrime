b2a3905dd6dad7e1378554f556fc3c64e0ddbea9
bryparker <brydon.parker@mail.utoronto.ca>
2021-03-10 10:41:43 -0500

    adding hexagonal chart on sample - still need to get it working on proper sample

--- streamlit_map_app.py
+++ streamlit_map_app.py
@@ -1,9 +1,39 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 st.title("Toronto Crime Analysis")
 
 crime_df = pd.read_csv("./data/processed/crime_data.csv").rename(columns={"long": "lon"})
 
-st.map(crime_df[["lat", "lon"]])
+# st.map(crime_df[["lat", "lon"]]) 
+
+st.pydeck_chart(pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+            latitude=43.65,
+            longitude=-79.38,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+               'HexagonLayer',
+               data=crime_df.head(100),
+               get_position='[lon, lat]',
+               radius=200,
+            #    elevation_scale=4,
+            #    elevation_range=[0, 1000],
+            #    pickable=True,
+            #    extruded=True,
+            ),
+#             pdk.Layer(
+#                 'ScatterplotLayer',
+#                 data=df,
+#                 get_position='[lon, lat]',
+#                 get_color='[200, 30, 0, 160]',
+#                 get_radius=200,
+#             ),
+        ],
+    ))
