726fe78398bf32f49288a89c4dfa1ed05810c0c0
bryparker <brydon.parker@mail.utoronto.ca>
2021-03-10 16:25:14 -0500

    additions to the clustering app - addresses - better labellning - location filtering

--- streamlit_clustering_app.py
+++ streamlit_clustering_app.py
@@ -7,6 +7,8 @@ with st.echo(code_location='below'):
     import hdbscan
     from pandasql import sqldf
     from plotnine import *
+    from geopy.geocoders import Nominatim
+    geolocator = Nominatim(user_agent="toronto_crime_app")
 
     #-----------------setup
     st.title("Toronto Crime Analysis")
@@ -36,17 +38,26 @@ with st.echo(code_location='below'):
             "Assault", "Robbery"
         ]
     )
+    location_options = st.sidebar.multiselect(
+        label="Choose Location Types",
+        options=crime_df.premisetype.unique().tolist(),
+        default=[
+            "Outside"
+        ]
+    )
 
     filtered_crime_df = crime_df[crime_df.neighbourhood.isin(nbhd_options)]
-    filtered_crime_df = filtered_crime_df[filtered_crime_df.crime_type.isin(crime_options)]
-
+    filtered_crime_df = filtered_crime_df[filtered_crime_df.crime_type.isin(
+        crime_options)]
+    filtered_crime_df = filtered_crime_df[filtered_crime_df.premisetype.isin(
+        location_options)]
 
     #--------------clustering
     st.sidebar.markdown('### Choose hdbscan parameters')
     st.sidebar.text("(see here for more details: https://hdbscan.readthedocs.io/en/latest/parameter_selection.html)")
     min_cluster_size = st.sidebar.selectbox(
         label="Choose Minimum Cluster Size",
-        options=[10, 50, 100, 500],
+        options=[10, 50, 100, 200, 400],
         index=2
     )
 
@@ -60,7 +71,7 @@ with st.echo(code_location='below'):
     # filter out the points that didn't get assigned a cluster
     filtered_crime_df_outliers_removed = filtered_crime_df[filtered_crime_df["cluster"] != -1]
     perc_data_removed_bc_of_outliers = (1 - filtered_crime_df_outliers_removed.shape[0] / filtered_crime_df.shape[0]) * 100
-    st.text(f"Percentage of crimes removed because they're considered outliers: {round(perc_data_removed_bc_of_outliers, 2)}%")
+    st.text(f"Percentage of crimes removed because they're considered outliers: {round(perc_data_removed_bc_of_outliers, 2)}% (choosing a lower Minimum Cluster Size will reduce this %")
 
     #--------------clustering eda
     pysqldf = lambda q: sqldf(q, globals())
@@ -83,10 +94,10 @@ with st.echo(code_location='below'):
         ), info_per_cluster_neighbourhood as (
             select 
                 cluster 
+                ,count(1) as n_per_cluster_and_neighbourhood
                 ,neighbourhood
                 ,population
                 ,sq_metres / 1000000 as sq_kms 
-                ,count(1) as n_per_cluster_and_neighbourhood
             from filtered_crime_df_outliers_removed
             group by cluster, neighbourhood
         ) 
@@ -103,15 +114,32 @@ with st.echo(code_location='below'):
             clust_and_nbhd.neighbourhood = nbhd.neighbourhood
         ;
     """
-    stats_per_cluster = pysqldf(query)
-    st.text('Cluster Statistics:')
+    stats_per_cluster_and_nbhd = pysqldf(query)
+    stats_per_cluster = stats_per_cluster_and_nbhd[["n_per_cluster", "cluster", "avg_lat", "avg_lon"]].drop_duplicates()
+    
+    #-------------extract the address from the lat and long
+    addresses = []
+    for ix, row in stats_per_cluster[["avg_lat", "avg_lon"]].iterrows():
+        address = geolocator.reverse(f"{row.avg_lat}, {row.avg_lon}")
+        addresses.append(address[0])
+    stats_per_cluster["Address"] = addresses
+
+    #---------------show dataframes
+    st.text('Cluster Statistics: (you can hover over the address to see more details)')
     st.dataframe(
-        stats_per_cluster
+        stats_per_cluster[["n_per_cluster", "cluster", "Address", "avg_lat", "avg_lon"]]
+        .sort_values(by="n_per_cluster", ascending=False)
+    )
+    st.text('Cluster/Neighbourhood Statistics:')
+    st.dataframe(
+        stats_per_cluster_and_nbhd
         .sort_values(by="n_per_cluster", ascending=False)
     )
 
     #-------visualization
-
+    if stats_per_cluster.shape[0] < 50: # => lets treat this as a str to viz it easier
+        filtered_crime_df_outliers_removed["cluster"] = filtered_crime_df_outliers_removed.cluster.astype(str)
+        stats_per_cluster["cluster"] = stats_per_cluster["cluster"].astype(str)
     p_clust = ggplot(
         filtered_crime_df_outliers_removed,
         aes("lon", "lat", color="cluster")
@@ -119,12 +147,15 @@ with st.echo(code_location='below'):
     st.text("Here are the crimes, coloured by their cluster after removing outliers")
     st.pyplot(p_clust.draw())
     p_clust_center = ggplot(
-        stats_per_cluster[["cluster", "avg_lat", "avg_lon", "n_per_cluster"]].drop_duplicates(),
+        stats_per_cluster,
         aes("avg_lon", "avg_lat", color="cluster", size="n_per_cluster")
     ) + geom_point()
     st.text("Here are the centers of each cluster, sized by how many crimes occurred in that cluster")
     st.pyplot(p_clust_center.draw())
-    p_nbhd = ggplot(filtered_crime_df_outliers_removed, aes("lon", "lat", color="neighbourhood")) + geom_point()
+    p_nbhd = ggplot(
+        filtered_crime_df_outliers_removed,
+        aes("lon", "lat", color="neighbourhood")) + geom_point()
+    st.text("Here are the crimes, coloured by their neighbourhood after removing outliers")
     st.pyplot(p_nbhd.draw())
 
     st.text("Here are the crimes on the streets of Toronto, you can use this to isolate the locations of each cluster represented above")
