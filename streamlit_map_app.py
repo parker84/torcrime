0b078a6be9a50b44f1ec9750b95576103db8f2b4
bryparker <brydon.parker@mail.utoronto.ca>
2021-03-10 09:41:52 -0500

    adding working version of streamlit map app (with no real data)

---
+++ streamlit_map_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Toronto Crime Analysis")
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
