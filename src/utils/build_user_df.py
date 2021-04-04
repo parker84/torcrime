698dc9ad23f51789a9b6aab4599f2ca6149a678c
Brydon Parker <brydon.parker@shopify.com>
2021-04-04 15:31:14 -0400

    testing build_user_df all passing

--- src/utils/build_user_df.py
+++ src/utils/build_user_df.py
@@ -8,23 +8,34 @@ from geopy.geocoders import Nominatim
 class BuildUserDf():
 
     def __init__(self):
+        self.geolocator = Nominatim(user_agent="toronto_crime_app")
         shopify.ShopifyResource.set_site(config("SHOP_URL"))
+    
+    def get_and_set_customer_df(self):
         customers = shopify.Customer.search()
         self.customer_df = pd.DataFrame([
             json.loads(customer.to_json().decode("utf-8").replace("'", '"'))["customer"]
             for customer in customers
         ])
-        self.geolocator = Nominatim(user_agent="toronto_crime_app")
     
+    def set_customer_df(self, customer_df):
+        self.customer_df = customer_df
+
     def build_user_df(self):
+        assert self.customer_df is not None, "you need to set your customer_df first"
         user_df = self._filter_to_paying_customer()
-        user_df = self._add_lat_lon_to_df(user_df)   
+        user_df = self._add_lat_lon_to_df(user_df)  
+        user_df = self._add_km_radius_for_30m_walking(user_df) 
         return user_df 
 
     def _filter_to_paying_customer(self):
+        # TODO: better handle users cancelling
         user_df = self.customer_df.dropna(axis=0, subset=["default_address"])[
             self.customer_df.verified_email
-        ][self.customer_df.orders_count > 0]
+        ][self.customer_df.orders_count > 0][
+            self.customer_df.state == "enabled"
+        ]
+        user_df = user_df[user_df.total_spent.astype(float) > 0]
         return user_df
     
     def _add_lat_lon_to_df(self, df1):
@@ -48,5 +59,11 @@ class BuildUserDf():
         df["lon"] = [
             loc.longitude for loc in locs
         ]
+        return df
+    
+    def _add_km_radius_for_30m_walking(self, df):
+        hours = .5
+        km_radius = round(hours * 5, 3) # we assume 5 km/h walk speed
+        df["km_radius"] = km_radius
         return df
 
--- tests/pytests/test_build_user_df.py
+++ tests/pytests/test_build_user_df.py
@@ -1,13 +1,58 @@
 import pytest
 from src.utils.build_user_df import BuildUserDf
 import pandas as pd
+import numpy as np
 
 @pytest.fixture
 def user_df():
-    '''Returns a Wallet instance with a zero balance'''
+    """Returns a user_df w 1 correct user and the rest that shouldn't be recieving emails
+
+    Returns:
+        [type]: [description]
+    """
+    default_address = {'id': 6319809265686, 'customer_id': 5126812467222, 'first_name': 'Brydon', 'last_name': 'Parker', 'company': '', 'address1': '12 Charlotte St', 'address2': 'Unit 501', 'city': 'Toronto', 'province': 'Ontario', 'country': 'Canada', 'zip': 'M5V 0M6', 'phone': '', 'name': 'Brydon Barker', 'province_code': 'ON', 'country_code': 'CA', 'country_name': 'Canada', 'default': False}
+    customer_df = pd.DataFrame({
+        "email": [
+            "test_correct_everything@gmail.com", 
+            "has_not_paid@gmail.com", 
+            "has_no_address@gmail.com", 
+            "has_not_verified_email@gmail.com", 
+            "has_no_orders@gmail.com", 
+            "state_not_enabled@gmail.com",
+            "has_no_spend@gmail.com", 
+        ],
+        "default_address": [
+            default_address,
+            default_address,
+            np.nan,
+            default_address,
+            default_address,
+            default_address,
+            default_address
+        ],
+        "verified_email": [True, True, True, False, True, True, True],
+        "total_spent": [1, 0, 1, 1, 1, 1, 0],
+        "orders_count": [1, 0, 1, 1, 0, 1, 1],
+        "state": ["enabled", "enabled", "enabled", "enabled", "enabled", "not_enabled", "enabled"]
+    })
     builder = BuildUserDf()
+    builder.set_customer_df(customer_df)
     user_df = builder.build_user_df()
     return user_df
 
-def test_email_users(user_df):
-    assert user_df.shape[0] > 0
+def test_user_filtering(user_df):
+    assert user_df.shape[0] == 1, "you should only have 1 user that made it through the filters"
+
+def test_address_extraction(user_df):
+    assert user_df.iloc[0]["tor_address"] == '12 Charlotte St, Toronto'
+
+def test_getting_customer_info():
+    builder = BuildUserDf()
+    builder.get_and_set_customer_df()
+    user_df = builder.build_user_df()
+    assert builder.customer_df.shape[0] > 0, "not extracting any customers from the shopify api"
+    if builder.customer_df.total_spent.sum() == 0:
+        assert user_df.shape[0] == 0, "you have rows in your user df even though no one has spent anything"
+
+def test_km_radius(user_df):
+    assert user_df.iloc[0].km_radius == 2.5, "km_radius is not correct"
