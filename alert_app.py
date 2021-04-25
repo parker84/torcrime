2ac884042170024159e71e2dbad007bdf5b87340
Brydon Parker <brydon.parker@shopify.com>
2021-04-25 15:15:49 -0400

    adding error catching to alert_app

--- alert_app.py
+++ alert_app.py
@@ -32,14 +32,25 @@ def append_to_streaming_raw_tweet_tables_every_n_secs(secs=10, log_every=1000):
     while True != False:
         if i % log_every == 0:
             logger.info(f'Making request number: {i+1}')
-        res_df = scrapper.get_tweets_from_last_n_secs("TPSOperations", secs, ops_tweet=True)
-        if res_df.shape[0] > 0:
-            get_user_df_and_send_emails(res_df)
-        scrapper.save_tweetdf_to_db(res_df, "streaming_raw_tps_ops_tweets", if_exists="append")
-        # res_df = scrapper.get_tweets_from_last_n_secs("TorontoPolice", secs)
-        # scrapper.save_tweetdf_to_db(res_df, "streaming_raw_to_police_tweets", if_exists="append")
-        i += 1
-        time.sleep(secs)
+        try: 
+            res_df = scrapper.get_tweets_from_last_n_secs("TPSOperations", secs, ops_tweet=True)
+            if res_df.shape[0] > 0:
+                get_user_df_and_send_emails(res_df)
+                scrapper.save_tweetdf_to_db(res_df, "streaming_raw_tps_ops_tweets", if_exists="append")
+            # res_df = scrapper.get_tweets_from_last_n_secs("TorontoPolice", secs)
+            # scrapper.save_tweetdf_to_db(res_df, "streaming_raw_to_police_tweets", if_exists="append")
+            i += 1
+            time.sleep(secs)
+        except Exception as err:
+            logger.error(f'Failed with error: {err}')
+            logger.info(f'Making request number: {i+1}, sleeping for 1m')
+            time.sleep(60)
+            # import ipdb; ipdb.set_trace()
+
+            res_df = scrapper.get_tweets_from_last_n_secs("TPSOperations", secs, ops_tweet=True)
+            if res_df.shape[0] > 0:
+                get_user_df_and_send_emails(res_df)
+            scrapper.save_tweetdf_to_db(res_df, "streaming_raw_tps_ops_tweets", if_exists="append")
 
 
 if __name__ == "__main__":
