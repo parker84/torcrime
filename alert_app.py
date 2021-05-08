fa0680df13293f6d10f4df650f4cef82894ddce4
Brydon Parker <brydon.parker@shopify.com>
2021-05-08 14:36:35 -0400

    switching the alert_app to write to the original db not just the streaming one - seems to be running fine

--- alert_app.py
+++ alert_app.py
@@ -34,21 +34,18 @@ def append_to_tables_and_email_users_latest_tweets_every_n_secs(secs=10, log_eve
             res_df = scrapper.get_tweets_from_last_n_secs("TPSOperations", secs, ops_tweet=True)
             if res_df.shape[0] > 0:
                 get_user_df_and_send_emails(res_df)
-                scrapper.save_tweetdf_to_db(res_df, "streaming_raw_tps_ops_tweets", if_exists="append")
-            # res_df = scrapper.get_tweets_from_last_n_secs("TorontoPolice", secs)
-            # scrapper.save_tweetdf_to_db(res_df, "streaming_raw_to_police_tweets", if_exists="append")
+                scrapper.save_tweetdf_to_db(res_df, "raw_tps_ops_tweets", if_exists="append")
             i += 1
             time.sleep(secs)
         except Exception as err:
             logger.error(f'Failed with error: {err}')
             logger.info(f'Making request number: {i+1}, sleeping for 1m')
             time.sleep(60)
-            # import ipdb; ipdb.set_trace()
 
             res_df = scrapper.get_tweets_from_last_n_secs("TPSOperations", secs, ops_tweet=True)
             if res_df.shape[0] > 0:
                 get_user_df_and_send_emails(res_df)
-            scrapper.save_tweetdf_to_db(res_df, "streaming_raw_tps_ops_tweets", if_exists="append")
+            scrapper.save_tweetdf_to_db(res_df, "raw_tps_ops_tweets", if_exists="append")
 
 
 if __name__ == "__main__":
